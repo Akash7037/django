9e7dbf6b5a506ed353e6e9e0576e3ae47133cc5c
Akash RE <arunaedwin6279@gmail.com>
2026-06-24 21:16:06 +0530

    Set DEBUG to False in settings.py
    Changed DEBUG setting to False for production.

--- project/project/settings.py
+++ project/project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)ulg8fa%^d3d1-t5ysy3a=!dbi8h%1dv(jzlwj8d#4se=*i926'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'django-msz3.onrender.com',
