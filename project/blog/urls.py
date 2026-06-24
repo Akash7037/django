b57110b2d659e9c881726fc050ebdb61cf8785b8
Akash RE <arunaedwin6279@gmail.com>
2026-06-24 21:15:01 +0530

    Update delete URL pattern for viewer posts

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -1,3 +1,3 @@
 from . import views
 from django.urls import path
-urlpatterns = [path("",views.home,name=""),path("viewer/<int:post>",views.show,name="viewer"),path("new",views.new,name="new"),path("delete/<int:bi>",views.delete,name="delete")]
+urlpatterns = [path("",views.home,name=""),path("viewer/<int:post>",views.show,name="viewer"),path("new",views.new,name="new"),path("viewer/delete/<int:bi>",views.delete,name="delete")]
