e46ac0857451d6cdbf8f6f39cb59cfcf5e1935f4
Akash RE <arunaedwin6279@gmail.com>
2026-06-24 22:20:18 +0530

    Modify data retrieval to order by date
    Updated queries to retrieve all data ordered by date in home, show, new, and delete views.

--- project/blog/views.py
+++ project/blog/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import user_passes_test
 from .models import data
 from .form import ProductForm
-con = data.objects.filter(published=True)
 def home(req):
+    con = data.objects.all().order_by('-date')
     return render(req,"home.html",{"con":con})
 def show(req,post):
+    con = data.objects.all().order_by('-date')
     dat = con.get(id=post)
     return render(req,"viewer.html",{"dat":dat})
 # Create your views here.
 @user_passes_test(lambda u: u.is_superuser)
 def new (req):
+    con = data.objects.all().order_by('-date')
     if req.method=="POST":
         form = ProductForm(req.POST)  
         if form.is_valid():
@@ -21,6 +23,7 @@ def new (req):
     return render (req,"new.html")
 @user_passes_test(lambda u: u.is_superuser)
 def delete(req,bi):
+    con = data.objects.all().order_by('-date')
     mod = data.objects.get(id=bi)
     mod.delete()
     render("/")
